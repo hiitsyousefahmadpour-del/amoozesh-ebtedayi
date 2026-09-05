d0c9dee17841788fd989e34041c7d5c63de169a1
hiitsyousefahmadpour-del <hiitsyousef.ahmadpour@gmail.com>
2026-09-05 11:19:24 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // اگر روی GitHub Pages منتشر می‌کنید، نام مخزن را اینجا بگذارید:
-  // base: '/my-repo-name/',
-  server: { port: 5173, open: true }
+  base: '/amoozesh-ebtedayi/',
+  server: {
+    port: 5173,
+    open: true
+  }
 })
